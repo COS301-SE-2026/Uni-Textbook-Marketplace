6318011a4f1f650f6fe6ef68bd74beba7ab03892
tl21thebe <tl21thebe@gmail.com>
2026-05-04 23:44:01 +0200

    chore: install ts-node and restore jest.config.ts for frontend

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,14 +2,14 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-    dir: './',
+  dir: './',
 })
 
 const config: Config = {
-    coverageProvider: 'v8',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    passWithNoTests: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  passWithNoTests: true,
 }
 
 export default createJestConfig(config)
