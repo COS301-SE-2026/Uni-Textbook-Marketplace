165c4b6e5a8296c187d2c70f26c22c42420d271b
nalediO <nalediomphemetse3@gmail.com>
2026-05-06 20:23:07 +0200

    Done with Button component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,2 +1,39 @@
+//Author: Omphemetse Mokgotahdi
 
+type ButtonProps = {
+    children: React.ReactNode;
+    variant?: "primary" | "secondary" | "danger";
+    disabled?: boolean;
+    onClick?: () => void;
+    className?: string;
+};
+
+
+export default function Button({
+    children,
+    variant = "primary",
+    disabled = false,
+    onClick,
+    className = "",
+
+}: ButtonProps) {
+
+    const varients = {
+        primary: "btn-primary ",
+        secondary: "btn-secondary",
+        danger: "btn-danger",
+    };
+
+    return (
+
+
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            className={`${varients[variant]} ${className}`}
+        >
+            {children}
+        </button>
+    );
+}
 
