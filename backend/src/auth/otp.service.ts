75372faafc6393df5ebb8518c6972faea6ef8a3d
GiftMHB <giftmohuba159@gmail.com>
2026-05-10 22:39:06 +0200

    completed otp service

--- backend/src/auth/otp.service.ts
+++ backend/src/auth/otp.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, BadRequestException } from "@nestjs/common";
 import {InjectRepository} from '@nestjs/typeorm';
 import * as crypto from 'crypto';
-import { Repository } from "typeorm";
+import { MoreThan, Repository } from "typeorm";
 
 import { User } from "./entities/user.entity";
 import { Otp } from './entities/otp.entity';
@@ -42,10 +42,60 @@ export class OtpService {
                 Date.now() + this.OTP_MINUTES * 60 * 1000
             ),
         });
-        
+
         await this.otpRepository.save(otp);
 
         return code;
+    }
+
+    async verifyOtp(email: string, submittedCode: string): Promise<void> {
+
+        const otp = await this.otpRepository.findOne({
+           where: {
+                email, 
+                used: false,
+                expires_at: MoreThan(new Date()),
+            },
+            order: {
+                created_at: 'DESC',
+            },
+        });
+
+
+        if(!otp){
+            throw new BadRequestException('No valid OTP found. Please request a new one.')
+        }
+
+        await this.otpRepository.increment(
+            {id: otp.id },
+            'attempts',
+            1,
+        );
+
+        if(otp.attempts + 1 >= this.MAX_ATTEMPTS){
+
+            await this.otpRepository.update(
+                {id: otp.id},
+                {used: true},
+            );
+
+            throw new BadRequestException('Too many failed attempts. Please request a new OTP.')
+
+        }
+
+        const isValid = crypto.timingSafeEqual(
+            Buffer.from(otp.code), 
+            Buffer.from(submittedCode),
+        );
+
+        if(!isValid){
+            throw new BadRequestException('Invalid OTP code.');
+        }
+
+        await this.otpRepository.update(
+            { id: otp.id },
+            { used: true },
+        );
     }
 
 }
