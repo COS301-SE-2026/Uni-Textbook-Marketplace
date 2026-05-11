33f3fbdc58c078a8e4ca6f024a27e4dfe968dba3
u23591732 <u23591732@tuks.co.za>
2026-05-11 14:54:35 +0200

    feat : added otps typeORM entity class and assertations

---
+++ database/entities/otps.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn
+} from 'typeorm';
+
+@Entity('otps')
+export class OTP {
+
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  email!: string;
+
+  @Column({
+    length: 6
+  })
+  code!: string;
+
+  @Column({
+    type: 'timestamptz'
+  })
+  expires_at!: Date;
+
+  @Column({
+    default: false
+  })
+  used!: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamptz'
+  })
+  created_at!: Date;
+}
