c7adfce58a206667a84bf47f93d2489b259fb0ae
JoshKretschmer <jekretschmer2003@gmail.com>
2026-05-11 16:54:24 +0200

    Listings controller post skeleton with swagger

--- backend/src/listings/listings.controller.ts
+++ backend/src/listings/listings.controller.ts
@@ -5,8 +5,37 @@ import {
   Param,
 } from '@nestjs/common';
 
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+
 import { ListingsService } from './listings.service';
 
+const HTTP_OK = 200;
+const HTTP_BAD_REQUEST = 400;
+const HTTP_NOT_FOUND = 404;
+const HTTP_CONFLICT = 409;
+const HTTP_INTERNAL_SERVER_ERROR = 500;
+
+@ApiTags('Listings')
 @Controller('listings')
 export class ListingsController {
+    constructor(
+        private readonly listingsService:ListingsService,
+    ){};
+
+    //Post listings
+    @Post()
+    @ApiOperation({
+        summary: 'Creating a new listing',
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'Listing submitted for review',
+    })
+    createListing() {
+        return this.listingsService.createListing();
+    }
 }
