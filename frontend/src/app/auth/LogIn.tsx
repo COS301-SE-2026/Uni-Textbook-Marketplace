6a33a5080250862d9c22971b539805f975d582fe
nalediO <nalediomphemetse3@gmail.com>
2026-05-12 05:08:05 +0200

    Added the logo for LogIn

--- frontend/src/app/auth/LogIn.tsx
+++ frontend/src/app/auth/LogIn.tsx
@@ -1,1 +1,28 @@
+
+"use client"
 import React, { useState } from 'react';
+
+
+export default function LoginPage(){
+
+
+    return(
+        <main className="min-h-screen flex items-center justify-center bg-background-light px-4">
+            <div className='card w-full max-w-5xl flex overflow-hidden'>
+
+                <div className='w-1/3 bg-cyan-50 p-8'>
+
+                    <div className='text-6xl mb-6'>
+                        
+                    </div>
+
+                </div>
+
+                <div className='flex-1 p-10'>
+
+                </div>
+
+            </div>
+        </main>
+    );
+}
