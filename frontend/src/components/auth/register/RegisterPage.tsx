6fe0a9ca03db29541646659045790769f24128c3
tl21thebe <tl21thebe@gmail.com>
2026-05-17 23:10:36 +0200

    feat: updated responsive switcher

--- frontend/src/components/auth/register/RegisterPage.tsx
+++ frontend/src/components/auth/register/RegisterPage.tsx
@@ -9,8 +9,10 @@ export default function RegisterPage() {
 
     useEffect(() => {
         const check = () => setIsMobile(window.innerWidth < 768);
+
         check();
         window.addEventListener("resize", check);
+
         return () => window.removeEventListener("resize", check);
     }, []);
 
