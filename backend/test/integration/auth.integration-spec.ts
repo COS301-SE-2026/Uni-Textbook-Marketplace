3293bd35fe85fb91752dbb7dc9d7af5c4f673bd7
GiftMHB <giftmohuba159@gmail.com>
2026-06-18 21:47:53 +0200

    done with auth/register tests

--- backend/test/integration/auth.integration-spec.ts
+++ backend/test/integration/auth.integration-spec.ts
@@ -1,13 +1,19 @@
 import { Test } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import { AppModule } from "../../src/app.module";
-import { DataSource } from "typeorm";
+import { DataSource, Repository } from "typeorm";
+import request from "supertest";
+
+import { University } from "../../src/database/entities/university.entity";
+import { User } from "../../src/database/entities/users.entity";
 
 
 describe('Auth Integration', () => {
 
     let app : INestApplication;
-    let dataSource : DataSource
+    let dataSource : DataSource;
+    let universityRepository : Repository<University>;
+    let userRepository : Repository<User>
 
     beforeAll(async () => {
         
@@ -20,11 +26,13 @@ describe('Auth Integration', () => {
         await app.init();
 
         dataSource = moduleRef.get(DataSource);
+        universityRepository = dataSource.getRepository(University);
+        userRepository = dataSource.getRepository(User);
     });
 
     afterEach(async () => {
         await dataSource.query(
-            'TRUNCATE TABLE users CASCADE'
+            'TRUNCATE TABLE universities, users CASCADE'
         )
     });
 
@@ -34,9 +42,74 @@ describe('Auth Integration', () => {
         }
     })
 
+    //for checking database connection
     describe('database', () => {
         it('should load app', () =>{
             expect(true).toBe(true);
         });
     });
+
+    //api/auth/register
+    it('should reject registration when university does not exist', async () =>{
+
+        const response = await request(app.getHttpServer())
+            .post('/auth/register')
+            .send({
+                email: `test${Date.now()}@test.tuks.co.za`,
+                password: 'student@123',
+                first_name: 'gift',
+                last_name: 'mohub',
+                university_id: '550e8400-e29b-41d4-a716-446655440000',
+                faculty: 'EBIT',
+            })
+            .expect(400);
+
+        expect(response.body).toBeDefined();
+    });
+
+    it('should reject email that does not match selected uni domain', async () =>{
+
+        const university = await universityRepository.save({
+            name: 'University of Pretoria',
+            email_domain: 'tuks.co.za'
+        });
+
+        const response = await request(app.getHttpServer())
+            .post('/auth/register')
+            .send({
+                email: `u1234598@tuks.com`,
+                password: 'student@123',
+                first_name: 'gift',
+                last_name: 'mohub',
+                university_id: university.id,
+                faculty: 'EBIT',
+            })
+            .expect(400);
+
+        expect(response.body).toBeDefined();
+    });
+
+    it('should register a student sucessfully', async () =>{
+        
+        const university = await universityRepository.save({
+            name: 'University of Pretoria',
+            email_domain: 'tuks.co.za'
+        }); 
+
+        const response = await request(app.getHttpServer())
+            .post('/auth/register')
+            .send({
+                email: `u1234598@tuks.co.za`,
+                password: 'student@123',
+                first_name: 'gift',
+                last_name: 'mohub',
+                university_id: university.id,
+                faculty: 'EBIT',
+            })
+            .expect(201);
+
+        expect(response.body).toBeDefined();
+    },15000);
+
+    
 });
