7ad624a48f6b5e756c6fc8de0b228e7ccf9140a4
GiftMHB <giftmohuba159@gmail.com>
2026-07-07 03:32:01 +0200

    feat: wishlist completed dto

---
+++ backend/src/wishlist/dto/wishlist.dto.ts
@@ -0,0 +1,17 @@
+import { IsUUID } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class WishlistDto {
+    
+    @ApiProperty({
+        example: '241447fc-e89d-4038-853c-2c2048df03d8',
+    })
+    @IsUUID()
+    user_id: string;
+
+    @ApiProperty({
+        example: '241447fc-e89d-4038-853c-2c2048df03d8',
+    })
+    @IsUUID()
+    listing_id: string;
+}
