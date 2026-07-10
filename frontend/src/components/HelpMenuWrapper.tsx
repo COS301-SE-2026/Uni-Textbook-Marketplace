cc129ac63e9e9d0e87060269e47568c6dc06158f
tl21thebe <tl21thebe@gmail.com>
2026-07-10 23:11:35 +0200

    feat: complete wrapper to hide help component

--- frontend/src/components/HelpMenuWrapper.tsx
+++ frontend/src/components/HelpMenuWrapper.tsx
@@ -3,6 +3,21 @@
 import { usePathname } from 'next/navigation'
 import HelpMenu from './HelpMenu'
 
+const RESTRICTED_ROUTES = [
+    '/',
+    '/admin/review',
+    '/auth/login',
+    '/auth/register',
+    '/auth/resetpassword'
+]
+
 export default function HelpMenuWrapper() {
 
+    const pathName = usePathname()
+
+    if (RESTRICTED_ROUTES.includes(pathName)) {
+        return null
+    }
+
+    return <HelpMenu />
 }
