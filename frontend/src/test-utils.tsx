ed2f291aba734cbbce3ad3329af8c2d0fabec156
tl21thebe <tl21thebe@gmail.com>
2026-07-18 16:22:32 +0200

    chore: added Dockerfile production for frontend

--- frontend/src/test-utils.tsx
+++ frontend/src/test-utils.tsx
@@ -1,23 +1,34 @@
 import { render, RenderOptions } from '@testing-library/react';
 import { ReactElement } from 'react';
 
+// @ts-nocheck
 Object.defineProperty(window, 'matchMedia', {
+
+
     writable: true,
     value: jest.fn().mockImplementation(query => ({
         matches: false,
+
         media:query,
         onchange: null,
+
+
         addListener: jest.fn(),
         removeListener: jest.fn(),
+
         addEventListener: jest.fn(),
         removeEventListener: jest.fn(),
+
         dispatchEvent: jest.fn(),
     })),
 });
 
 jest.mock('next-themes', () => ({
+
+
     useTheme: jest.fn(() => ({
         theme: 'light',
+
         setTheme: jest.fn(),
     })),
 }));
@@ -25,28 +36,36 @@ jest.mock('next-themes', () => ({
 jest.mock('lucide-react', () => ({
     Moon: () => <svg data-testid="moon-icon" />,
     Sun: () => <svg data-testid="sun-icon" />,
+
     Menu: () => <svg data-testid="menu-icon" />,
     X: () => <svg data-testid="x-icon" />,
+
     Bell: () => <svg data-testid="bell-icon" />,
     ChevronDown: () => <svg data-testid="chevron-down-icon" />,
+
     BookOpen: () => <svg data-testid="book-open-icon" />,
     Check: () => <svg data-testid="check-icon" />,
+
     Heart: () => <svg data-testid="heart-icon" />,
 }));
 
 jest.mock('next/navigation', () => ({
+
     usePathname: jest.fn(() => '/'),
     useRouter: jest.fn(() => ({
+
         push: jest.fn(),
         back: jest.fn(),
     })),
 }));
 
 jest.mock('next/image', () => ({
+
     __esModule: true,
     default: (props: any) => {
         // eslint-disable-next-line @next/next/no-img-element
         return <img {...props} alt={props.alt} />;
+
     },
 }));
 
@@ -54,4 +73,6 @@ const customRender = (ui: ReactElement, options?: RenderOptions) =>
     render(ui, { ...options });
 
 export * from '@testing-library/react';
+
+
 export { customRender as render };
