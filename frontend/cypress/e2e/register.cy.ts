78623dcbaeba0bcbf45f9b675232a22b58a3f0be
tl21thebe <tl21thebe@gmail.com>
2026-08-10 17:17:40 +0200

    test: registration stepper and password validation e2e test configured

--- frontend/cypress/e2e/register.cy.ts
+++ frontend/cypress/e2e/register.cy.ts
@@ -31,4 +31,44 @@ describe('Registration', () => {
         cy.contains('REGISTER').should('be.visible');
 
     });
+
+    it('blocks step 1 from advancing with empty requireed fields', () => {
+        cy.contains('button', 'Next').click();
+
+        cy.contains('Full name is required').should('be.visible');
+        cy.contains('Surname is required').should('be.visible');
+
+
+        cy.get('select[name=university]').should('not.exist');
+
+    });
+
+
+    it('blocks step 2 from advancing without a selected university', () => {
+        cy.get('input[placeholder="Enter your full name(s)"]').type('Cypress');
+        cy.get('input[placeholder="Enter your surname"]').type('Tester');
+
+        cy.contains('button', 'Next').click();
+        cy.contains('button', 'Next').click();
+        cy.contains('Please select your university').should('be.visible');
+    });
+
+
+    it('blocks step 2 from advancing when the email does not match selected university domain', () => {
+        cy.get('input[placeholder="Enter your full name(s)"]').type('Cypress');
+        cy.get('input[placeholder="Enter your surname"]').type('Tester');
+
+
+        cy.contains('button', 'Next').click();
+        cy.get('select[name="university"]').select(universityName);
+        cy.get('input[placeholder="@university.email"]').type('cypress.test@university.gmail.com');
+        cy.contains('button', 'Next').click();
+        cy.contains(`Email must end in @${emailDomain}`).should('be.visible');
+
+    });
+
+
+    it('blocks step 3 from advancing when the passwords do not match', () => {
+
+    });
 })
