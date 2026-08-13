27199be706b0efda73b1370014082d88123682b0
GiftMHB <giftmohuba159@gmail.com>
2026-08-13 11:34:28 +0200

    feat : imports

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ListingNotificationListener } from './listing.notification.listener';
 
 @Module({})
 export class NotificationsModule {}
--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { AdminEvent } from '../admin/events/admin.event';
 
 @Injectable()
 export class NotificationsService {}
