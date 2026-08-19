3f0e97d0ca07978d8aa9f9b305d854bcc9c06096
GiftMHB <giftmohuba159@gmail.com>
2026-08-19 15:05:01 +0200

    feat : messaging listener

--- backend/src/notifications/listeners/listing.notification.listener.ts
+++ backend/src/notifications/listeners/listing.notification.listener.ts
@@ -3,6 +3,7 @@ import { OnEvent } from '@nestjs/event-emitter';
 import { AdminEvent } from '../../admin/events/admin.event';
 import { NotificationsService } from '../notifications.service';
 import { EditEvent } from '../../listings/events/edit.event';
+import { MessageEvent } from '../../messaging/events/message.event';
 
 @Injectable()
 export class ListingNotificationListener {
@@ -16,5 +17,10 @@ export class ListingNotificationListener {
   @OnEvent('listing.edit')
   notifyAdminofEdit(event: EditEvent) {
     this.notificationsService.notifyAdmin(event);
+  }
+
+  @OnEvent('messaging')
+  notifyStudentofMessage(event: MessageEvent){
+    this.notificationsService.notifyStudentofMessage(event);
   }
 }
