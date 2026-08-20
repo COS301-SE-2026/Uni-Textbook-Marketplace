500bcd3b15f76861a3f868c61d923a3fd9064a74
tl21thebe <tl21thebe@gmail.com>
2026-08-20 02:01:05 +0200

    feat: prettier dto for listing status

--- backend/src/listings/dto/updateListingStatus.dto.ts
+++ backend/src/listings/dto/updateListingStatus.dto.ts
@@ -2,6 +2,6 @@ import { IsEnum } from 'class-validator';
 import { ListingsStatus } from 'src/database/entities/listing.entity';
 
 export class UpdateListingStatusDto {
-    @IsEnum(ListingsStatus)    
-    listing_status: ListingsStatus;
+  @IsEnum(ListingsStatus)
+  listing_status: ListingsStatus;
 }
