e2411638a981a91198c08bd3552a2095a3054a23
GiftMHB <giftmohuba159@gmail.com>
2026-08-28 22:51:36 +0200

    feat: extended the IEmailService

--- backend/src/email/email.interface.ts
+++ backend/src/email/email.interface.ts
@@ -1,5 +1,6 @@
 export interface IEmailService {
   sendOtp(to: string, otp: string): Promise<void>;
+  sendNotificationEmail(to: string, entityType:string,data: any)
 }
 
 export const EMAIL_SERVICE = 'EMAIL_SERVICE';
