ab1801fdaee10755dccc1b78e99d6df8e2fb11b6
GiftMHB <giftmohuba159@gmail.com>
2026-08-30 03:18:59 +0200

    feat: added the dependency

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -5,6 +5,8 @@ import { NotificationsController } from './notifications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notifications } from '../database/entities/notifications.entity';
 import { SavedSearchMatchListener } from './listeners/saved-search-match.listener';
+import { EMAIL_SERVICE } from '../email/email.interface';
+import { MailtrapEmailProvider } from '../email/mailtrap-email.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notifications])],
@@ -13,7 +15,11 @@ import { SavedSearchMatchListener } from './listeners/saved-search-match.listene
     NotificationsService,
     ListingNotificationListener,
     SavedSearchMatchListener,
+    {
+      provide: EMAIL_SERVICE,
+      useClass: MailtrapEmailProvider,
+    },
   ],
   exports: [NotificationsService],
 })
-export class NotificationsModule {}
+export class NotificationsModule { }
