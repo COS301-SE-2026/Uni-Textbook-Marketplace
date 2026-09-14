d335acecd0167d07f5aa030b81887b0b2224c1cc
u23591732 <u23591732@tuks.co.za>
2026-09-14 09:03:52 +0200

    feat : security vulnerabilities bug fixes -> add global validation pipe to enforce dto type checking

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   app.use(cookieParser());
 
