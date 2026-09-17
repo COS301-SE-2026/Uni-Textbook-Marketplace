7d985f96aa6c3c5f23762a48ede9ec1d92487d72
tl21thebe <tl21thebe@gmail.com>
2026-09-17 17:18:33 +0200

    feat: completed azure vision module with module calls to service and controller

---
+++ backend/src/vision/vision.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Book } from '../database/entities/book.entity';
+import { VisionController } from './vision.controller';
+import { VisionService } from './vision.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Book])],
+  controllers: [VisionController],
+  providers: [VisionService],
+})
+export class VisionModule {}
