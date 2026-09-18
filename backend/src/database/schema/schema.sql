4c3169d810b3a7d027f8f1019dde7fb1943e7226
GiftMHB <giftmohuba159@gmail.com>
2026-09-19 00:16:53 +0200

    feat : adde bid table

--- backend/src/database/schema/schema.sql
+++ backend/src/database/schema/schema.sql
@@ -205,18 +205,28 @@ CREATE TABLE cases (
 --auction
 CREATE TABLE auction (
     id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    listings_id UUID NOT NULL REFERENCES listings(id) ON DELETE SET NULL,
-    seller_id UUID NOT NULL REFERENCES users(id) ON DELETE SET NULL,
-    starting_price number NOT NULL,
-    reserve_price number,
-    current_highest_bid number,
-    current_highest_bidder_id UUID NOT NULL REFERENCES users(id) ON DELETE SET NULL,
+    listings_id UUID REFERENCES listings(id) ON DELETE SET NULL,
+    seller_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    starting_price NUMERIC NOT NULL,
+    reserve_price NUMERIC,
+    current_highest_bid NUMERIC,
+    current_highest_bidder_id UUID  REFERENCES users(id) ON DELETE SET NULL,
     start_time TIMESTAMPTZ,
     end_time TIMESTAMPTZ,
     status VARCHAR(20) DEFAULT 'SCHEDULED' CHECK (status IN ('SCHEDULED','ACTIVE','ENDED', 'CANCELLED'))
+    extension_count INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
 
-
+CREATE TABLE bid (
+    id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    auction_id UUID REFERENCES auction(id) ON DELETE SET NULL,
+    bidder_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    amount NUMERIC NOT NULL,
+    placed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    status VARCHAR(20) NOT NULL CHECK (status IN ('ACCEPTED','REJECTED')),
+    rejection_reason TEXT,
+)
 
 
 
