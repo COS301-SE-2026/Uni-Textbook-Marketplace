0a2a018b2a3cd4ca5f1bb780901301d75eb0c6c3
GiftMHB <giftmohuba159@gmail.com>
2026-09-21 11:46:16 +0200

    feat : place bid endpoint

--- backend/src/auction/auction.controller.ts
+++ backend/src/auction/auction.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrentUser } from '../auth/decorator/user.decorator';
@@ -6,11 +6,13 @@ import { CreateAuctionDto } from './dto/CreateAuctionDto';
 import { AuctionService } from './auction.service';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorator/roles.decorator';
+import { PlaceBidDto } from './dto/PlaceBidDto';
+import { User } from '../database/entities/users.entity';
 
 @ApiTags('Auctions')
 @ApiBearerAuth()
 @Controller('auction')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard,RolesGuard)
 export class AuctionController {
 
     constructor(
@@ -22,9 +24,18 @@ export class AuctionController {
         summary: 'Create an auction',
         description: 'Creates an auction for an approved listing',
     })
-    @UseGuards(RolesGuard)
     @Roles('student')
-    createAuction(@Body() dto: CreateAuctionDto,@CurrentUser('id') userId: string,) {
+    async createAuction(@Body() dto: CreateAuctionDto,@CurrentUser('id') userId: string,) {
         return this.auctionService.createAuction(dto, userId);
     }
+
+    @Post(':id/bid')
+    @ApiOperation({
+        summary: 'place a bid',
+    })
+    @Roles('student')
+    async placeBid(@Param('id') auctionId: string, @Body() dto: PlaceBidDto, @CurrentUser() user: User){
+        return this.auctionService.placeBid(auctionId, user.id, dto.amount);
+    }
+
 }
