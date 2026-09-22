4d012c6e2f5c61817e9b799f6ed7a7ae7bdb8af8
GiftMHB <giftmohuba159@gmail.com>
2026-09-22 19:21:52 +0200

    feat: added bid in auction module

--- backend/src/auction/auction.module.ts
+++ backend/src/auction/auction.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auction } from '../database/entities/auction.entity';
+import { Bid } from '../database/entities/bid.entity';
 import { Listing } from '../database/entities/listing.entity';
 import { AuctionService } from './auction.service';
 import { AuctionController } from './auction.controller';
@@ -10,7 +11,7 @@ import { AuctionProcessor } from './auction.processor';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Auction, Listing,User]),
+        TypeOrmModule.forFeature([Auction, Bid, Listing,User]),
         BullModule.registerQueue({ name: 'auction' }),
     ],
     controllers: [AuctionController],
